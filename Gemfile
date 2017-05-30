4d28e08e6eb5c106b7fd84d8104cddc98e6cd476
Daniella Montinola <dmonti15@gmail.com>
2017-05-30 00:16:31 -0500

    add comments

--- Gemfile
+++ Gemfile
@@ -34,6 +34,8 @@ gem 'jbuilder', '~> 2.5'
 gem 'devise'
 gem 'bootstrap-sass', '~> 3.3.6'
 gem 'acts_as_votable', '~> 0.10.0'
+gem 'simple_form', '~> 3.5'
+gem 'record_tag_helper', '~> 1.0'
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
+  resources :comments
   devise_for :users
   resources :links do
     member do
       put "like", to: "links#upvote"
       put "dislike", to: "links#downvote"
     end
+    # by using "resources" it will do all of the RESTFUL routes for you
+    resources :comments
   end 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
